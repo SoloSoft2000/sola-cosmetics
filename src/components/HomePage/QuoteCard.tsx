52d12f052b92615eca0b7c50bec8eb5646c7545d
Eugene Solomonyk <solosoft2000@mail.ru>
2025-02-20 16:28:30 +0200

    refactor QuoteCard

--- src/components/HomePage/QuoteCard.tsx
+++ src/components/HomePage/QuoteCard.tsx
@@ -1,82 +1,71 @@
 "use client";
 
-import { useEffect, useState } from "react";
 import clsx from "clsx";
 import { useLocale } from "next-intl";
-import { RefreshCcw } from 'lucide-react';
-import { Locale } from '../../i18n/config';
+import { RefreshCcw } from "lucide-react";
+import { Locale } from "../../i18n/config";
+import { useAsync, useToggle } from "react-use";
+import { useState } from "react";
 
-type Quote = { name: string; text: string };
+type QuoteType = { name: string; text: string };
 
-const fetchNewQuote = async (lng: Locale, prevQuote: Quote | null) => {
+const fetchNewQuote = async (lng: Locale, prevQuote?: QuoteType): Promise<QuoteType> => {
   const response = await fetch(`/api/quote?lng=${lng}`);
-  const newQuote = await response.json() as Quote;
-  if (newQuote === prevQuote) return fetchNewQuote(lng, prevQuote);
-  return newQuote;
+  const newQuote = (await response.json()) as QuoteType;
+  return newQuote.text !== prevQuote?.text ? newQuote : fetchNewQuote(lng, prevQuote);
 };
 
 const QuoteCard = () => {
-  const [quote, setQuote] = useState<Quote | null>(null);
-  const [isAnimateChangeQuote, setAnimateChangeQuote] = useState(false);
-  const [isFirstLoad, setIsFirstLoad] = useState(true);
+  const [quote, setQuote] = useState<QuoteType>({name: 'Author', text: 'Quote Text'});
+  const [isAnimate, toggleAnimate] = useToggle(false);
+  const [isFirstMount, setNotFirstMount] = useState(true);
 
   const lng = useLocale() as Locale;
-  const isHebrew =  lng === 'he';
+  const isHebrew = lng === "he";
 
-  useEffect(() => {
-    const loadFirstQuote = async () => {
-      const firstQuote = await fetchNewQuote(lng, null);
-      setQuote(firstQuote);
-    };
-    loadFirstQuote();
+  const { loading, error } = useAsync(async () => {
+    const newQuote = await fetchNewQuote(lng);
+    setQuote(newQuote);
   }, [lng]);
 
   const handleQuoteUpdate = async () => {
-    if (isAnimateChangeQuote) return;
-    setIsFirstLoad(false);
-    setAnimateChangeQuote(true);
+    if (isAnimate) return;
+    setNotFirstMount(false);
+    toggleAnimate(true);
     const newQuote = await fetchNewQuote(lng, quote);
     setTimeout(() => {
       setQuote(newQuote);
-      setAnimateChangeQuote(false);
+      toggleAnimate(false);
     }, 1200);
   };
 
   return (
-    <div
-      className="relative cursor-pointer"
-      onClick={handleQuoteUpdate}
-    >
+    <div className="relative cursor-pointer" onClick={handleQuoteUpdate}>
       <div className="relative">
         <div
           className={clsx(
             "h-16 md:h-24 p-1 md:p-4 transition duration-500 ease-in-out",
-            !isFirstLoad ? isAnimateChangeQuote ? "animateLeftQuote" : "animateRightQuote" : ''
+            loading && "opacity-50 animate-pulse",
+            !isFirstMount && (isAnimate ? "animateLeftQuote" : "animateRightQuote")
           )}
-        > 
-          <blockquote
-            className={clsx(
-              "text-sm md:text-xl italic text-primary flex",
-              isHebrew && "text-right",
-              !isHebrew && "text-left"
-            )}
-          >
-            
-            {quote?.text ? `"${quote.text}"` : '...'}
-            
+        >
+          <blockquote className={clsx("text-sm md:text-xl italic text-primary flex", isHebrew ? "text-right" : "text-left")}>
+            {error ? `Error: ${error.message}` : quote?.text}
           </blockquote>
         </div>
         <div
           className={clsx(
             "bottom-1 text-base px-4 font-semibold text-gray-600 md:absolute transition duration-500 ease-in-out",
-            isHebrew && "md:left-2",
-            !isHebrew && "md:right-2",
-            !isFirstLoad ? isAnimateChangeQuote ? "animateLeftQuote" : "animateRightQuote" : ''
+            isHebrew ? "md:left-2" : "md:right-2",
+            loading && "opacity-50 animate-pulse",
+            !isFirstMount && (isAnimate ? "animateLeftQuote" : "animateRightQuote")
           )}
         >
-          {quote?.name || '...'}
+          {quote?.name}
         </div>
-        <div className={clsx("absolute bottom-2 md:top-4 text-primary animate-spin-slow w-6 h-6", isHebrew ? 'left-4' : 'right-4')}><RefreshCcw /></div>
+        <div className={clsx("absolute bottom-2 md:top-4 text-primary animate-spin-slow w-6 h-6", isHebrew ? "left-4" : "right-4")}>
+          <RefreshCcw />
+        </div>
       </div>
     </div>
   );
